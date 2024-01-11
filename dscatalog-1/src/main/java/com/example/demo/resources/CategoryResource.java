fa51c4d6612f43574d4d9cf3a27445215c43556a
unknown <matheusquevedobio@gmail.com>
2024-01-11 01:25:45 -0300

    Add method Post to categories

--- dscatalog-1/src/main/java/com/example/demo/resources/CategoryResource.java
+++ dscatalog-1/src/main/java/com/example/demo/resources/CategoryResource.java
@@ -1,12 +1,17 @@
 package com.example.demo.resources;
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.example.demo.services.CategoryService;
 import com.example.demo.dto.CategoryDTO;
 import com.example.demo.entities.Category;
@@ -32,6 +37,13 @@ public class CategoryResource {
 		CategoryDTO dto = service.findById(id); 
 		return ResponseEntity.ok().body(dto);
 		
+	}
+	
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 
 }
