e0d06f6ffb305d538c4ea528840459f4334b5cd3
unknown <matheusquevedobio@gmail.com>
2024-01-12 15:36:58 -0300

    Product Entity SQL Script

--- dscatalog-1/src/main/java/com/example/demo/entities/Category.java
+++ dscatalog-1/src/main/java/com/example/demo/entities/Category.java
@@ -1,25 +1,32 @@
 package com.example.demo.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name= "tb_category")
 public class Category implements Serializable {
-
-	
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
 	
 	public Category() {
 		
