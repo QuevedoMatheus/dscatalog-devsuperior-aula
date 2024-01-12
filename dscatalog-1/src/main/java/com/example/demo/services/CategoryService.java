dea99b904a3b67d2a676393147d874742befaeb6
unknown <matheusquevedobio@gmail.com>
2024-01-11 23:18:34 -0300

    Delete Category

--- dscatalog-1/src/main/java/com/example/demo/services/CategoryService.java
+++ dscatalog-1/src/main/java/com/example/demo/services/CategoryService.java
@@ -6,12 +6,15 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.dto.CategoryDTO;
 import com.example.demo.entities.Category;
 import com.example.demo.repositories.CategoryRepository;
+import com.example.demo.services.exceptions.DataBaseException;
 import com.example.demo.services.exceptions.ResourceNotFoundException;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -54,6 +57,19 @@ public class CategoryService {
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id não encontrado" + id);
 		}
+		
+	}
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id não encontrado" + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataBaseException("Violação de Integridade" + id);
+		}
+		
 		
 	}
 }
